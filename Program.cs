e2515f4bd58a98e2d1e15f1ae70fa2e48e0f33d4
RusUsf <ruslan@usf.edu>
2023-08-28 09:45:31 -0400

    Worked until on p.22

--- Program.cs
+++ Program.cs
@@ -77,6 +77,47 @@ Console.WriteLine(pointOne.X); // 9
 Console.WriteLine(pointTwo.X); // 7
 
 // Reference Type
+PointClass pointClassOne = new PointClass();
+pointClassOne.X = 7;
+
+PointClass pointClassTwo = pointClassOne; // Copies reference
+
+Console.WriteLine(pointClassOne.X); // 7
+Console.WriteLine(pointClassTwo.X); // 7
+
+pointClassOne.X = 9;                // Change pointClassOne.X
+Console.WriteLine(pointClassOne.X); // 9
+Console.WriteLine(pointClassTwo.X); // 9
+
+// Nullable Value Type
+
+int? i = null;
+Console.WriteLine(i==null);             // True
+
+//Struct
+Nullable<int> iI = new Nullable<int>();
+Console.WriteLine(!iI.HasValue);        // True
+
+// Nullable Reference Type
+var myString = "Retrieve from memory!";
+Foo(myString);
+void Foo(string? s)
+{
+    // Safe guard for NullReferenceException
+    if(s != null) Console.WriteLine(s.Length); 
+    if(s !=null) Console.WriteLine(s);
+}
+
+// Numeric suffixes F & M                               p.21
+float f = 4.5F;     // Won't compile without suffix
+decimal d = -1.23M; // Won't compile without suffix
+
+// Numeric Conversions
+int myX = 12345;        // int is a 32-bit integral type
+long myY = myX;         // Implicit conversion to 64-bit int
+short myZ = (short)myX; // Explicit conversion to 16-bit int
+
+
 
 
 
--- obj/Debug/net8.0/Pocket_CS_9.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pocket_CS_9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pocket_CS_9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd3ecd19ff03046e6eb504e2897ab35cd7797a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828ee0fb41247135ec9ed5a420bff6ddef42ffd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pocket_CS_9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pocket_CS_9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
